4e1384830049ef1990bade820903d759f7ea3d1d
hcedeno81 <hcedeno81@gmail.com>
2026-06-23 22:28:36 -0500

    Update App.jsx
    pasa hacer mas rapido el sistema

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import * as XLSX from "xlsx";
 import { loadData, saveData, subscribeData } from "./db";
 
